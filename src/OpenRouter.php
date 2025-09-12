6e1d24a06021d6dd03cc28f19b44dbef2efcf66d
Luis Güette <guetteluis@gmail.com>
2025-09-12 17:56:12 -0400

    feat: integrate HandlesCompletions trait into OpenRouter class

--- src/OpenRouter.php
+++ src/OpenRouter.php
@@ -2,8 +2,12 @@
 
 namespace Taecontrol\OpenRouter;
 
+use Taecontrol\OpenRouter\Concerns\HandlesCompletions;
+
 class OpenRouter
 {
+    use HandlesCompletions;
+
     protected OpenRouterConnector $connector;
 
     public function __construct(?string $token = null)

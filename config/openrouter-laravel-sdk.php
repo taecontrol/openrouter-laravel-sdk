9539877fd0522777300ca3914d27212443c99e91
Luis Güette <guetteluis@gmail.com>
2025-09-12 16:33:21 -0400

    add OpenRouterConnector and update configuration settings

--- config/openrouter-laravel-sdk.php
+++ config/openrouter-laravel-sdk.php
@@ -2,5 +2,7 @@
 
 // config for Taecontrol/OpenRouter
 return [
-
+    'base_uri' => 'https://openrouter.ai/api/v1',
+    'connect_timeout' => 10,
+    'request_timeout' => 120,
 ];
---
+++ src/Commands/OpenRouterConnector.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Taecontrol\OpenRouter\Commands;
+
+use Saloon\Http\Connector;
+use Saloon\Traits\Plugins\HasTimeout;
+
+class OpenRouterConnector extends Connector
+{
+    use HasTimeout;
+
+    /**
+     * @inheritDoc
+     */
+    public function resolveBaseUrl(): string
+    {
+        return config('openrouter-laravel-sdk.base_uri');
+    }
+
+    public function getConnectTimeout(): float
+    {
+        return config('openrouter-laravel-sdk.connect_timeout');
+    }
+
+    public function getRequestTimeout(): float
+    {
+        return config('openrouter-laravel-sdk.request_timeout');
+    }
+}
